f3158370139a583ef7dbefcd75dbdc265d0269d8
Camila Menezes <mii.menezesb08@gmail.com>
2020-08-31 20:11:07 -0300

    Adicionadas e validadas novas colunas em orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :service
-
+  validates :final_price, presence: true
+  validates :reservation, presence: true
+  validates :status, presence: true, inclusion: { in: ["Confirmado", "Em espera", "Cancelado"] }
 end
